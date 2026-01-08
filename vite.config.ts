fc0d3c2dbfa5c2c1796f802a7c212d4cc13c4520
sidcaulfieldd <cau0006@gmail.com>
2026-01-08 20:07:54 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/bio-wiki-style/', //
+  base: '/bio-wiki-style/',
   server: {
     host: "::",
     port: 8080,
