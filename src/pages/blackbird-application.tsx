b6742d2874f2d99a232d7f57aa569f1a38ed5484
sidcaulfieldd <cau0006@gmail.com>
2026-09-06 14:37:50 +1000

    Update blackbird-application.tsx

--- src/pages/blackbird-application.tsx
+++ src/pages/blackbird-application.tsx
@@ -402,7 +402,18 @@ export default function Scratch() {
     drawCurrentFrame();
     showMuteOverlay();
 
-    preloadFrames().then(() => {
+    function preloadVideo() {
+      return new Promise<void>((resolve) => {
+        if (video.readyState >= 2) {
+          resolve();
+          return;
+        }
+        video.addEventListener("loadeddata", () => resolve(), { once: true });
+        video.addEventListener("error", () => resolve(), { once: true });
+      });
+    }
+
+    Promise.all([preloadFrames(), preloadVideo()]).then(() => {
       resizeCanvas();
       hideLoader();
       initScrollTrigger();
