19ba03cdd47a9a0f85f36e9a8b61737189d1be50
sidcaulfieldd <cau0006@gmail.com>
2026-09-20 13:16:46 +0000

    center dance section, add outlined frames

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -183,7 +183,9 @@ const Index = () => {
               {/* Dance gif-scrub -> video, same pin/scrub effect as the Blackbird page,
                   boxed to a centered 16:9 stage instead of a full-bleed portrait takeover. */}
               <div className="my-6">
-                <DanceScroll />
+                <div className="flex justify-center md:translate-x-[150px]">
+                  <DanceScroll />
+                </div>
               </div>
             </div>
 
