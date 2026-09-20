782507da3e01dfbf04d47a3d839235aa86f139c5
sidcaulfieldd <cau0006@gmail.com>
2026-09-20 19:20:29 +1000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -237,7 +237,7 @@ const WalkingSid = ({ cardRef }: { cardRef: RefObject<HTMLDivElement> }) => {
             muted
             playsInline
             className="block w-full h-full object-cover"
-            style={{ transform: "scale(1.02)" }}
+            style={{ transform: "scale(1.01)" }}
             onEnded={(e) => {
               e.currentTarget.pause();
               setShowLeft(false);
@@ -254,7 +254,7 @@ const WalkingSid = ({ cardRef }: { cardRef: RefObject<HTMLDivElement> }) => {
             muted
             playsInline
             className="block w-full h-full object-cover"
-            style={{ transform: "scale(1.02)" }}
+            style={{ transform: "scale(1.01)" }}
             onEnded={(e) => {
               e.currentTarget.pause();
               setShowRight(false);
