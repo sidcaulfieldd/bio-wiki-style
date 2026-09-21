279ddbe915161eb5f010360750959d79e6e361b7
sidcaulfieldd <cau0006@gmail.com>
2026-09-21 11:35:55 +1000

    Update DanceScroll.tsx

--- src/components/DanceScroll.tsx
+++ src/components/DanceScroll.tsx
@@ -343,7 +343,7 @@ export default function DanceScroll() {
     >
       <div
         ref={boxRef}
-        className="md:translate-x-[150px]"
+        className="md:translate-x-[174px] rounded-lg"
         style={{
           position: "relative",
           width: 270,
